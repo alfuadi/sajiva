7465e4c4cca13a718d28a6fd2f20be9ee67d2610
alfuadi <57789372+alfuadi@users.noreply.github.com>
2024-04-05 19:08:34 +0700

    Update Sajiva_2024.py

--- Sajiva_2024.py
+++ Sajiva_2024.py
@@ -140,8 +140,7 @@ def plot_data(selvar, startdate, enddate):
 
             rows_to_drop1 = []
             for i in range(1, len(df) - 1):
-                if abs(float(df[param].iloc[i-1])  - float(df[param].iloc[i+1]))/abs(float(df[param].iloc[i])  - float(df[param].iloc[i+1])) < 0.1 or \
-                abs(float(df[param].iloc[i+1])  - float(df[param].iloc[i+2]))/abs(float(df[param].iloc[i])  - float(df[param].iloc[i+1])) < 0.1:
+                if abs(float(df[param].iloc[i])  - float(df[param].iloc[i+1]))/abs(float(df[param].iloc[i+1])  - float(df[param].iloc[i+2])) < 10:
                     rows_to_drop1.append(i)
             df_cleaned1 = df.drop(index=rows_to_drop1).reset_index(drop=True)
             
@@ -167,7 +166,7 @@ def plot_data(selvar, startdate, enddate):
 
             rows_to_drop = []
             for i in range(1, len(df) - 1):
-                if abs(float(df[param].iloc[i-1])  - float(df[param].iloc[i+1]))/abs(float(df[param].iloc[i])  - float(df[param].iloc[i+1])) < 0.1:
+                if abs(float(df[param].iloc[i])  - float(df[param].iloc[i+1]))/abs(float(df[param].iloc[i+1])  - float(df[param].iloc[i+2])) < 10:
                     rows_to_drop.append(i)
             df_cleaned = df.drop(index=rows_to_drop).reset_index(drop=True)
     
